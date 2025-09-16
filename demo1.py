05f273697ed6c4f978334653d5a6a40d3b1032fe
LayomiG <layomi.giwa@gmail.com>
2025-09-16 09:14:57 -0600

    First commit

---
+++ demo1.py
@@ -0,0 +1,17 @@
+x = 3
+y = 4 
+
+game_id = False 
+user_name = "layo"
+password_entry = 5
+welcome_message = "welcome back user"
+error_code = "Password incorrect"
+
+if password_entry == 5:
+    print (x)
+    print(welcome_message)
+else:
+    print (y)
+    print (error_code)
+
+
